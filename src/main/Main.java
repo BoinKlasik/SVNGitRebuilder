17575171601aa76f9c1b85d5d1c96f1da597c975
nickrak <nickrak.nr@gmail.com>
2014-04-19 09:31:38 -0700

    Added support to add a third parameter which loads known file extensions from a file.
    --------------
    txt: ascii
    exe: binary

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,9 +4,13 @@ import git.Blob;
 import git.Commit;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map.Entry;
+import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
 
@@ -18,14 +22,13 @@ public class Main
 
 	/**
 	 * git update-server-info is required to ennsure .git/info/refs file exists
-	 * 
+	 *
 	 * @param args
 	 * @throws IOException
 	 * @throws InterruptedException
 	 */
 	public static void main(String [] args) throws IOException, InterruptedException
 	{
-		loadKnownExtensions();
 		if (args.length < 2)
 		{
 			usage();
@@ -34,6 +37,11 @@ public class Main
 
 		sourceRepo = args[0];
 		targetRepo = args[1];
+
+		if (args.length > 3)
+		{
+			loadKnownExtensions(args[2]);
+		}
 
 		Runtime time = Runtime.getRuntime();
 		//this ensures that .git/info/refs file exists (easier than traversing the whole damn .git/refs folder
@@ -72,10 +80,31 @@ public class Main
 		System.out.println("known files: " + Blob.knownExtensions);
 	}
 
-	private static void loadKnownExtensions()
+	private static void loadKnownExtensions(String propertiesFile)
 	{
-		// TODO Auto-generated method stub
+		final File f = new File(propertiesFile);
+		final Properties props = new Properties();
 
+		try (InputStream in = new FileInputStream(f))
+		{
+			props.load(in);
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+			System.exit(1);
+			return;
+		}
+
+		for (Entry<Object, Object> entry : props.entrySet())
+		{
+			Blob.knownExtensions.add(entry.getKey().toString());
+
+			if (entry.getValue().equals("binary"))
+			{
+				Blob.binaryExtensions.add(entry.getKey().toString());
+			}
+		}
 	}
 
 	private static void usage()
